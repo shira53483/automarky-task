55d2667bc004d78559b072323cb71524fec13235
shira53483 <shiratkohen@gmail.com>
2025-09-08 02:48:56 +0300

    addEnv

---
+++ frontend/src/environment/environment.production.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: true,
+  apiUrl: 'https://magic-link-backend.onrender.com/api',
+  backendUrl: 'https://magic-link-backend.onrender.com', 
+  frontendUrl: 'FRONTEND_URL_PLACEHOLDER'
+};
